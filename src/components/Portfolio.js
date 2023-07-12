09f7892b1e5faa1f00dfa24190495a2376c8a0dc
maaront <35611834+maaront@users.noreply.github.com>
2023-07-12 19:30:47 -0400

    fix routing issue for home page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,7 +12,7 @@ export default function Portfolio() {
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === "Home") {
-      return <Hero />;
+      return <Home />;
     }
     if (currentPage === "Work") {
       return <Work />;

c10c73ad592fd267f0c8a718ff90247147156bd0
maaront <35611834+maaront@users.noreply.github.com>
2023-07-12 15:29:22 -0400

    initialize npm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import '../public/css/App.css';
-import portfolio from './components/portfolio';
-import hero from './components/hero';
-import work from './components/work';
-import about from './components/about';
-import referrals from './components/referrals';
-import contact from './components/contact';
-import nav from './components/nav';
+import Portfolio from './components/Portfolio';
 
-function App() {
-  return (
-    <div className="portfolio">
-      <Portfolio />
-    </div>
-  );
-}
+const App = () => <Portfolio />;
 
 export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// The ReactDOM.render method is used to render a react element into the actual DOM
+ReactDOM.render(<App />, document.getElementById('root'));

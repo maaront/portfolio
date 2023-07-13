917d1a45549bb3d5623f8f87559adbd93dd2c121
maaront <35611834+maaront@users.noreply.github.com>
2023-07-12 20:40:55 -0400

    add work initial content

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,14 +1,13 @@
 import React from 'react';
-
-
-export default function Home() {
+export default function Work() {
   return (
     <div>
-        <h1>
-        I MAKE
-      </h1>
-      <h1>DIGITAL PRODUCTS</h1>
-      <img src="../public/assets/down-arrow.svg" alt="arrow" />
+        <h2>
+        WORK
+      </h2>      
+      <card>leaflink</card>
+      <card>baseball weather</card>
+      <card>yard to table</card>
     </div>
   );
 }

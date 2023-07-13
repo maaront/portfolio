a514aadec286ebe05425aa38e14b9f616a939aba
maaront <35611834+maaront@users.noreply.github.com>
2023-07-12 20:39:38 -0400

    add Home initial content

---
+++ src/components/Work.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Home() {
+  return (
+    <div>
+        <h1>
+        I MAKE
+      </h1>
+      <h1>DIGITAL PRODUCTS</h1>
+      <img src="../public/assets/down-arrow.svg" alt="arrow" />
+    </div>
+  );
+}

f1454cf06e49bb9beef72906bd3ae53b07993de4
maaront <35611834+maaront@users.noreply.github.com>
2023-07-13 14:11:54 -0400

    add initial contact content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div>
-      <h2>CONTACT</h2>
+    <div className="page-container" id="contact-container">
+      <h2 className="headline" id="headline_contact">CONTACT</h2>
       <form onSubmit={(event) => event.preventDefault()}>
         <label htmlFor="name">My name is </label>
         <input type="text" id="name" name="name" required />
         <span>, </span>
-        <br />
+        
 
         <label htmlFor="email">my email is </label>
         <input type="email" id="email" name="email" required />
         <span>, </span>
-        <br />
+        
 
         <label htmlFor="subject">and I’d like to talk about </label>
         <input type="text" id="subject" name="subject" required />

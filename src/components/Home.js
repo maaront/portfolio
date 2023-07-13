85e8b94e2be4a4b05837735b3b66da5c5b2d333a
maaront <35611834+maaront@users.noreply.github.com>
2023-07-12 22:55:12 -0400

    home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
-import React from 'react';
-
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-        <h1>
-        I MAKE
-      </h1>
-      <h1>DIGITAL PRODUCTS</h1>
+      <h1 id="headline-1">I MAKE</h1>
+      <div className="mask-text">
+        <h1 id="headline-2">DIGITAL <br></br>PRODUCTS</h1>
+      </div>
       <img src="/assets/down-arrow.svg" alt="arrow" />
     </div>
   );

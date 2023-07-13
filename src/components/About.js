2110fea5ea2783d385da0f4a6447c7a8a8fb3022
maaront <35611834+maaront@users.noreply.github.com>
2023-07-12 21:10:06 -0400

    add initial referall and about content

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default function About() {
+  return (
+    <div>
+        <h2>
+        About
+      </h2>      
+      <img src='' alt=''/>
+      <p>I'm Matt Turner, a creative and constructive project and product manager with 12 years experience leading teams. My professional experience touches on almost all aspects of the software space, including development, design, content, marketing, product owner, scrum master, and project management.</p>
+    </div>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,7 +19,7 @@ function Nav({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="#work"
-          onClick={() => handlePageChange('work')}
+          onClick={() => handlePageChange('Work')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
         >
@@ -43,7 +43,7 @@ function Nav({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Referrals' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Referrals
         </a>
       </li>
       <li className="nav-item">

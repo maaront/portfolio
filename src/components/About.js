3832da89dbb5fc7f627042f56ec02197b4709fe1
maaront <35611834+maaront@users.noreply.github.com>
2023-07-14 15:43:40 -0400

    styling tweaks for About content

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 export default function About() {
   return (
     <div className="page-container">
-    <h2 className="headline" id="">
-      ABOUT
-    </h2>
-    <section id="about-content">
-      <img className="headshot" src="assets/photos/referrals/matt-turner-nobg.png" alt="Matthew Turner headshot" />
-      <p>I'm <span>Matt Turner</span>, a creative and constructive project and product manager with 12 years experience leading teams.{"\n"}{"\n"}{"\n"}My professional experience touches on almost all aspects of the software space, including development, design, content, marketing, product owner, scrum master, and project management.</p>
+      <h2 className="headline" id="">
+        ABOUT
+      </h2>
+      <section id="about-content">
+        <img
+          className="headshot"
+          src="assets/photos/referrals/matt-turner-nobg.png"
+          alt="Matthew Turner headshot"
+        />
+        <section className="about-text">
+          <p>
+            I'm <span className="red">Matt Turner</span>, a creative and constructive project
+            and product manager with 12 years experience leading teams.
+          </p>
+          <br></br>
+          <p>
+            My professional experience touches on almost all aspects of the
+            software space, including development, design, content, marketing,
+            product owner, scrum master, and project management.
+          </p>
+        </section>
       </section>
     </div>
   );
 }
-
-

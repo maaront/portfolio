8d3f12e9233adfef816a69484d6839794178d92c
maaront <35611834+maaront@users.noreply.github.com>
2023-07-14 10:37:10 -0400

    make highlight make a tag in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,31 @@
 import React from 'react';
 
 
+//Function to keep the highlight div the same width as the <a> tag
+// Select all elements with the class "nav-item"
+const navItems = document.querySelectorAll('.nav-item');
+
+// Loop through each "nav-item"
+navItems.forEach((item) => {
+  // Get the highlight div and sibling <a> tag within the current "nav-item"
+  const highlight = item.querySelector('#highlight');
+  const link = item.querySelector('a');
+
+  // Add event listener for when mouse enters the "nav-item"
+  item.addEventListener('mouseenter', () => {
+    // Set the width of the highlight div to match the width of the <a> tag
+    highlight.style.width = `${link.offsetWidth}px`;
+  });
+
+  // Add event listener for when mouse leaves the "nav-item"
+  item.addEventListener('mouseleave', () => {
+    // Set the width of the highlight div back to 0
+    highlight.style.width = '0';
+  });
+});
+
+
+
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function Nav({ currentPage, handlePageChange }) {

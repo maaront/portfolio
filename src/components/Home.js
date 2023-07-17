c33f9a18d4c7bf3d43ded75787cf095001e71e4a
maaront <35611834+maaront@users.noreply.github.com>
2023-07-16 22:13:00 -0400

    add mutliple headline items and cycle through

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
+const headlines = ['DIGITAL PRODUCTS', 'WEB APPLICATIONS', 'MOBILE APPS', 'UI/UX DESIGNS'];
+
 export default function HomePage() {
-  
   const [showHeadline, setShowHeadline] = useState(false);
   const [showMaskText, setShowMaskText] = useState(false);
   const [showArrow, setShowArrow] = useState(false);
+  const [currentHeadlineIndex, setCurrentHeadlineIndex] = useState(0);
 
   useEffect(() => {
     setShowHeadline(true);
     setTimeout(() => setShowMaskText(true), 500); // Delay of 500ms
     setTimeout(() => setShowArrow(true), 1000); // Delay of 1000ms
+
+    const interval = setInterval(() => {
+      setCurrentHeadlineIndex((prevIndex) => (prevIndex + 1) % headlines.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="home-container">
       <CSSTransition in={showHeadline} timeout={1000} classNames="fade" unmountOnExit>
-        <h1 className="headline">My name is Matt,<br></br>and I make</h1>
+        <h1 className="headline">My name is Matt,<br />and I make</h1>
       </CSSTransition>
       <CSSTransition in={showMaskText} timeout={500} classNames="fade" unmountOnExit>
         <div className="mask-text">
-          <h1 id="headline-2">DIGITAL <br></br>PRODUCTS</h1>
+          <h1 id="headline-2">{headlines[currentHeadlineIndex]}</h1>
         </div>
       </CSSTransition>
       <CSSTransition in={showArrow} timeout={500} classNames="slide-down" unmountOnExit>

a2462fa29a312d22ffaa7de7fad1a406362e183f
maaront <35611834+maaront@users.noreply.github.com>
2023-07-17 21:50:27 -0400

    add more work sections for AC

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,49 +5,78 @@ export default function Work() {
     <div className="page-container" id="work-container">
       <h2 className="headline">WORK</h2>
 
+      <div className="work-grid">
+        <div className="card-container" id="baseball-schedule">
+          <div className="overlay">
+            <div className="items"></div>
+            <div className="items head">
+              <h3>Baseball Tracker</h3>
+            </div>
+            <div className="items tech">
+              <p>OPEN WEATHER // ESPN STATS // JS</p>
+            </div>
+          </div>
+        </div>
 
-<div className="work-grid">
-      <div className="card-container" id="baseball-schedule">
-  <div className="overlay">
-    <div className = "items"></div>
-    <div className = "items head">
-      <h3>Baseball Tracker</h3>
-      
-    </div>
-    <div className = "items tech">
-      <p>OPEN WEATHER // ESPN STATS // JS</p>
-    </div>
-</div>
-</div>
+        <div className="card-container" id="leaf-link">
+          <div className="overlay">
+            <div className="items"></div>
+            <div className="items head">
+              <h3>LeafLink</h3>
+            </div>
+            <div className="items tech">
+              <p>NODE // EXPRESS // TAILWIND // SEQUELIZE // HANDLEBARS</p>
+            </div>
+          </div>
+        </div>
 
-<div className="card-container" id="leaf-link">
-  <div className="overlay">
-    <div className = "items"></div>
-    <div className = "items head">
-      <h3>LeafLink</h3>
-      
-    </div>
-    <div className = "items tech">
-      <p>NODE // EXPRESS // TAILWIND // SEQUELIZE // HANDLEBARS</p>
-    </div>
-</div>
-</div>
+        <div className="card-container" id="yard-to-table">
+          <div className="overlay">
+            <div className="items"></div>
+            <div className="items head">
+              <h3>Yard To Table</h3>
+            </div>
+            <div className="items tech">
+              <p>REACT // MONGOOSE // NODE // EXPRESS</p>
+            </div>
+          </div>
+        </div>
+        <div className="card-container" id="baseball-schedule">
+          <div className="overlay">
+            <div className="items"></div>
+            <div className="items head">
+              <h3>Baseball Tracker</h3>
+            </div>
+            <div className="items tech">
+              <p>OPEN WEATHER // ESPN STATS // JS</p>
+            </div>
+          </div>
+        </div>
 
-<div className="card-container" id="yard-to-table">
-  <div className="overlay">
-    <div className = "items"></div>
-    <div className = "items head">
-      <h3>Yard To Table</h3>
-      
-    </div>
-    <div className = "items tech">
-      <p>REACT // MONGOOSE // NODE // EXPRESS</p>
-    </div>
-</div>
-</div>
+        <div className="card-container" id="leaf-link">
+          <div className="overlay">
+            <div className="items"></div>
+            <div className="items head">
+              <h3>LeafLink</h3>
+            </div>
+            <div className="items tech">
+              <p>NODE // EXPRESS // TAILWIND // SEQUELIZE // HANDLEBARS</p>
+            </div>
+          </div>
+        </div>
 
-</div>
-
+        <div className="card-container" id="yard-to-table">
+          <div className="overlay">
+            <div className="items"></div>
+            <div className="items head">
+              <h3>Yard To Table</h3>
+            </div>
+            <div className="items tech">
+              <p>REACT // MONGOOSE // NODE // EXPRESS</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
